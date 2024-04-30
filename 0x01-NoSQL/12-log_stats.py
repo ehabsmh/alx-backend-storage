c72350efe3b75f86c42c2e080cad9312a944e2cd
ehabsmh <ehabsmh3@gmail.com>
2024-04-30 21:51:27 +0300

    main only

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -1,23 +1,23 @@
 #!/usr/bin/env python3
 import pymongo
 
+if __name__ == "__main__":
+    client = pymongo.MongoClient("mongodb://127.0.0.1:27017")
+    db = client.logs
 
-client = pymongo.MongoClient("mongodb://127.0.0.1:27017")
-db = client.logs
-
-print(db.nginx.count_documents({}), "logs")
-print("Methods:")
-count_get_methods = db.nginx.count_documents({"method": "GET"})
-count_post_methods = db.nginx.count_documents({"method": "POST"})
-count_put_methods = db.nginx.count_documents({"method": "PUT"})
-count_patch_methods = db.nginx.count_documents({"method": "PATCH"})
-count_delete_methods = db.nginx.count_documents({"method": "DELETE"})
-print("\t method GET:", count_get_methods)
-print("\t method POST:", count_post_methods)
-print("\t method PUT:", count_put_methods)
-print("\t method PATCH:", count_patch_methods)
-print("\t method DELETE:", count_delete_methods)
-print(db.nginx.count_documents({
-    "method": "GET",
-    "path": "/status"
-    }), "status check")
+    print(db.nginx.count_documents({}), "logs")
+    print("Methods:")
+    count_get_methods = db.nginx.count_documents({"method": "GET"})
+    count_post_methods = db.nginx.count_documents({"method": "POST"})
+    count_put_methods = db.nginx.count_documents({"method": "PUT"})
+    count_patch_methods = db.nginx.count_documents({"method": "PATCH"})
+    count_delete_methods = db.nginx.count_documents({"method": "DELETE"})
+    print("\t method GET:", count_get_methods)
+    print("\t method POST:", count_post_methods)
+    print("\t method PUT:", count_put_methods)
+    print("\t method PATCH:", count_patch_methods)
+    print("\t method DELETE:", count_delete_methods)
+    print(db.nginx.count_documents({
+        "method": "GET",
+        "path": "/status"
+        }), "status check")
