7b2b2dfd90339ad7c5f0610553bb6cf0a1736199
ehabsmh <ehabsmh3@gmail.com>
2024-04-30 22:56:36 +0300

    14. Top students

--- 0x01-NoSQL/101-students.py
+++ 0x01-NoSQL/101-students.py
@@ -1,9 +1,8 @@
 #!/usr/bin/env python3
 """14. Top students"""
-from pymongo.collection import Collection
 
 
-def top_students(mongo_collection: Collection):
+def top_students(mongo_collection):
     """Returns all students sorted by average score"""
     return mongo_collection.aggregate([
         {'$addFields': {'averageScore': {'$avg': "$topics.score"}}},
