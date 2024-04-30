93b9e16a8dc6ef9ba8eb3670c821957c15fd8a4b
ehabsmh <ehabsmh3@gmail.com>
2024-04-30 21:23:52 +0300

    11. Where can I learn Python?

--- 0x01-NoSQL/11-schools_by_topic.py
+++ 0x01-NoSQL/11-schools_by_topic.py
@@ -1,10 +1,8 @@
 #!/usr/bin/env python3
 """11. Where can I learn Python?"""
-from pymongo.collection import Collection
-from typing import List
 
 
-def schools_by_topic(mongo_collection: Collection, topic: str) -> List[object]:
+def schools_by_topic(mongo_collection, topic):
     """Returns the list of school having a specific topic"""
 
     return mongo_collection.find({"topics": topic})
