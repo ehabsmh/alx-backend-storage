9e2c586dfc5a82cb8b7999bfc868372a77fac4af
ehabsmh <ehabsmh3@gmail.com>
2024-05-01 22:48:42 +0300

    0. Writing strings to Redis

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -1,22 +1,22 @@
 #!/usr/bin/env python3
 """0. Writing strings to Redis"""
 import redis
-from uuid import UUID, uuid4
+from uuid import uuid4
 from typing import Union
 
 
 class Cache:
     """Representing caching in memory"""
-    def __init__(self):
+    def __init__(self) -> None:
         """Stores an instance of the Redis client as a private variable"""
         self._redis = redis.Redis()
         self._redis.flushdb()
 
     # ________________________________________________________________________________
 
-    def store(self: "Cache", data: Union[str, bytes, int, float]):
+    def store(self, data: Union[str, bytes, int, float]) -> str:
         """Generates a random key and assign a value to it
         Returns the key"""
-        key: UUID = str(uuid4())
+        key = str(uuid4())
         self._redis.set(key, data)
         return key
