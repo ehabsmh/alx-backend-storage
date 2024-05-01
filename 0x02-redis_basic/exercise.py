5f59eebc8dc9f4f753e564e916f353b8ff065649
ehabsmh <ehabsmh3@gmail.com>
2024-05-01 23:22:41 +0300

    .

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -1,107 +1,83 @@
 #!/usr/bin/env python3
 """
-Redis module
+A module that creates a cache class to store redis instances
 """
-import sys
-from functools import wraps
-from typing import Union, Optional, Callable
-from uuid import uuid4
-
 import redis
-
-UnionOfTypes = Union[str, bytes, int, float]
+import uuid
+from typing import Union, Callable, Optional, Any
+from functools import wraps
 
 
 def count_calls(method: Callable) -> Callable:
     """
-    a system to count how many
-    times methods of the Cache class are called.
-    :param method:
-    :return:
+    A system that counts how many times methods of the Cache class are called
     """
     key = method.__qualname__
 
     @wraps(method)
     def wrapper(self, *args, **kwargs):
-        """
-        Wrap
-        :param self:
-        :param args:
-        :param kwargs:
-        :return:
-        """
+        """wrap"""
         self._redis.incr(key)
         return method(self, *args, **kwargs)
-
     return wrapper
 
 
 def call_history(method: Callable) -> Callable:
-    """
-    add its input parameters to one list
-    in redis, and store its output into another list.
-    :param method:
-    :return:
-    """
+    """Function that add its input parametes one list in redis"""
     key = method.__qualname__
-    i = "".join([key, ":inputs"])
-    o = "".join([key, ":outputs"])
+    inputs = key + ":inputs"
+    outputs = key + ":outputs"
 
     @wraps(method)
     def wrapper(self, *args, **kwargs):
-        """ Wrapp """
-        self._redis.rpush(i, str(args))
-        res = method(self, *args, **kwargs)
-        self._redis.rpush(o, str(res))
-        return res
+        """wrap"""
+        self._redis.rpush(inputs, str(args))
+        data = method(self, *args, **kwargs)
+        self._redis.rpush(outputs, str(data))
+        return data
+    return wraper
 
-    return wrapper
+
+def replay(method: Callable) -> None:
+    """Replays the history of a function"""
+    name = method.__qualname__
+    cache = redis.Redis()
+    calls = cache.get(name).decode("utf-8")
+    print("{} was called {} times:".format(name, calls))
+    inputs = cache.lrange(name + ":inputs", 0, -1)
+    outputs = cache.lrange(name + ":outputs", 0, -1)
+    for i, o in zip(inputs, outputs):
+        print("{}(*{}) -> {}".format(name, i.decode('utf-8'),
+                                     o.decode('utf-8')))
 
 
 class Cache:
-    """
-    Cache redis class
-    """
-
-    def __init__(self):
-        """
-        constructor of the redis model
-        """
+    def __init__(self) -> None:
+        """Initializes the class Cache"""
         self._redis = redis.Redis()
         self._redis.flushdb()
 
     @count_calls
     @call_history
-    def store(self, data: UnionOfTypes) -> str:
-        """
-        generate a random key (e.g. using uuid),
-         store the input data in Redis using the
-          random key and return the key.
-        :param data:
-        :return:
-        """
-        key = str(uuid4())
-        self._redis.mset({key: data})
+    def store(self, data: Union[str, bytes, int, float]) -> str:
+        """Stores data in redis caches"""
+        key = str(uuid.uuid4())
+        self.__redis.set(key, data)
+
         return key
 
-    def get(self, key: str, fn: Optional[Callable] = None) \
-            -> UnionOfTypes:
-        """
-        convert the data back
-        to the desired format
-        :param key:
-        :param fn:
-        :return:
-        """
-        if fn:
-            return fn(self._redis.get(key))
+    def get(self, key: str, fn: Optiopnal[Callable] = None)\
+            -> Union[str, bytes, int, float, None]:
+        """Get data from the reddis Cache"""
         data = self._redis.get(key)
+        if data is not None and fn is not none and callable(fn):
+            return data
+
+    def get_str(self, key: str) -> str:
+        """Get data as a string from redis cache"""
+        data = self.get(key, lambda x: x.decode('utf-8'))
         return data
 
-    def get_int(self: bytes) -> int:
-        """get a number"""
-        return int.from_bytes(self, sys.byteorder)
-
-    def get_str(self: bytes) -> str:
-        """get a string"""
-        return self.decode("utf-8")
+    def get_int(self, key: str) -> int:
+        """Get data as integer from redis cache"""
+        return self.get(key, int)
--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,34 +1,53 @@
 #!/usr/bin/env python3
-""" expiring web cache module """
-
-import redis
+"""A module that tracks the live of caches"""
 import requests
-from typing import Callable
+import redis
 from functools import wraps
+from typing import Callable
 
-redis = redis.Redis()
 
+def count_access(method: Callable) -> Callable:
+    """A function that checks and update the cache time"""
+    @wraps(method)
+    def wrapper(*args, **kwargs) -> str:
+        """updates cache time"""
+        url: str = arg[0]
+        count_key: str = f"count:{url}"
+        cache_key: str = f"cache:{url}"
 
-def wrap_requests(fn: Callable) -> Callable:
-    """ Decorator wrapper """
+        access_count: int = redis_client.incr(count_key)
 
-    @wraps(fn)
-    def wrapper(url):
-        """ Wrapper for decorator guy """
-        redis.incr(f"count:{url}")
-        cached_response = redis.get(f"cached:{url}")
-        if cached_response:
-            return cached_response.decode('utf-8')
-        result = fn(url)
-        redis.setex(f"cached:{url}", 10, result)
+        print(f"Access count for {url}: {access_count}")
+        cached_result: bytes = redis_client.get(cache_key)
+        if cached_result:
+            return cached_result.decode("utf-8")
+
+        result: str = method(*args, **kwargs)
+        redis_client.set(count_key, 0)
+        redis_client.setex(cache_key, 10, result)
+
         return result
-
     return wrapper
 
 
-@wrap_requests
+@count_access
 def get_page(url: str) -> str:
-    """get page self descriptive
-    """
+    """ A function that send http request to the supplied url"""
     response = requests.get(url)
     return response.text
+
+
+if __name__ == "__main__":
+    redis_client: redis.Redis = redis.Redis()
+    slow_url: str = ("http://slowwly.robertomurray.co.uk"
+                     "/delay/1000/url/https://www.example.com")
+    print(get_page(slow_url))
+    print(get_page(slow_url))
+
+    fast_url: str = ("https://github.com/millywanderi/alx-backend-storage/"
+                     "blob/main/0x02-redis_basic/exercise.py")
+    print(get_page(fast_url))
+
+    import time
+    time.sleep(11)
+    print(get_page(slow_url))
